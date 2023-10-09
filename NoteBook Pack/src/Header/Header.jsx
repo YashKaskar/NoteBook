d47d2b2adfb06412726ebb84258d837dd246cfc1
Yash Kaskar <yash.kaskar99@gmail.com>
2023-10-09 15:40:07 +0530

    Index..html and header

--- NoteBook Pack/src/Header/Header.jsx
+++ NoteBook Pack/src/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (  
     <>
       <div className='bg-[#ffea92] flex justify-center'>  
-      <img src={logo} alt="logo"  className='w-[13%] bg-[#ffea92]'/> 
+      <img src={logo} alt="logo"  className='w-[25%] md:w-[13%] bg-[#ffea92]'/> 
      </div>
       
     </>
