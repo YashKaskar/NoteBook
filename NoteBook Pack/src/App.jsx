a3f7b055760461225520c0e3b70269cc0dbe7506
Yash Kaskar <yash.kaskar99@gmail.com>
2023-10-16 16:20:09 +0530

    Main Content

--- NoteBook Pack/src/App.jsx
+++ NoteBook Pack/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header/Header"
+import MainContent from "./MainContent/content"
 
 function App() {
   
 
   return (
     <>
-    <Header />
+      <Header />
+      <MainContent />
     </>
   )
 }
