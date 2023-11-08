904e261bccb017227f5700056ed2876a225d8960
Yash Kaskar <yash.kaskar99@gmail.com>
2023-11-08 11:30:55 +0530

    content

--- NoteBook Pack/src/MainContent/Content/Content.jsx
+++ NoteBook Pack/src/MainContent/Content/Content.jsx
@@ -5,6 +5,7 @@ const Content = () => {
     <div className='w-[280px] h-[250px] flex p-[12px] flex-col  bg-cyan-400'>
       <textarea className='flex-[1] resize-none bg-transparent text-[1rem] outline-none border-none' />
       <p>Time</p>
+      
     </div>
  
   )
