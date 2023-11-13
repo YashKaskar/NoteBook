799233596a8f1a3305dd639b636dc162124db114
Yash Kaskar <yash.kaskar99@gmail.com>
2023-11-13 15:59:00 +0530

    css

--- NoteBook Pack/src/MainContent/Content/Content.jsx
+++ NoteBook Pack/src/MainContent/Content/Content.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = () => {
   return (
-    <div className='w-[280px] h-[250px] flex p-[12px] flex-col  bg-cyan-400'>
+    <div className='w-[280px] h-[250px] flex p-[12px] flex-col  bg-cyan-400 mt-10'>
       <textarea className='flex-[1] resize-none bg-transparent text-[1rem] outline-none border-none' />
       <p>Time</p>
       
