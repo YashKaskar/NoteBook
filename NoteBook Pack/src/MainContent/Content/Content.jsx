b931e8c2ab802648c8e46ed4f5248a3cdbc77bf0
Yash Kaskar <yash.kaskar99@gmail.com>
2023-11-14 07:35:11 +0530

    Content

--- NoteBook Pack/src/MainContent/Content/Content.jsx
+++ NoteBook Pack/src/MainContent/Content/Content.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = (props) => {
   return (
-    <div className='w-[280px] h-[250px] flex p-[12px] flex-col  bg-cyan-400 mt-10'>
+    <div className='w-[280px] h-[250px] flex p-[12px] flex-col  bg-cyan-400'>
       <textarea className='flex-[1] resize-none bg-transparent text-[1rem] outline-none border-none' />
       <p>{props.note.time}</p>
       
--- NoteBook Pack/src/MainContent/ContentContainer/ContentContainer.jsx
+++ NoteBook Pack/src/MainContent/ContentContainer/ContentContainer.jsx
@@ -3,10 +3,22 @@ import Content from '../Content/Content'
 
 const ContentContainer = () => {
   return (
-    <div>
+    <div className='h-[120%]'>
+      <div className='flex flex-wrap gap-10 justify-center'>
       <Content note={{
           time: "4.24"
       }} />
+      <Content note={{
+          time: "4.24"
+      }} />
+      <Content note={{
+          time: "4.24"
+      }} />
+      <Content note={{
+          time: "4.24"
+        }} />
+       
+      </div>
     </div>
   )
 }
