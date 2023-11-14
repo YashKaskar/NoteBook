7798239b36dc520aca6a09bd549279044416750d
Yash Kaskar <yash.kaskar99@gmail.com>
2023-11-14 07:35:39 +0530

    Header

--- NoteBook Pack/src/Header/Header.jsx
+++ NoteBook Pack/src/Header/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo.svg'
 const Header = () => {
   return (  
     <>
-      <div className='bg-[#ffea92] flex justify-center'>  
+      <div className='bg-[#ffea92] flex justify-center sticky top-0'>  
       <img src={logo} alt="logo"  className='w-[25%] md:w-[13%] bg-[#ffea92]'/> 
      </div>
       
